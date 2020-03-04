ea9d56b00f36367be2ffa5c427ac0360bf78244a
Piyojo <piyojo@gmail.com>
2020-03-04 12:56:30 +0100

    loading sound and making todo

--- setup.js
+++ setup.js
@@ -1,4 +1,14 @@
 
+funtion preload() {
+    sol = loadSound('./_sound/selfoscalation-long.mp3');
+    sos = loadSound('./_sound/selfoscalation-short.mp3');
+}
+
+
+// todo: setVolume with slider for both 
+// todo: play stop button for both 
+// todo: spiral icon? 
+// todo: some kind of loop?
 function setup() {
 
 }
