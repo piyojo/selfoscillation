20a6975a16cb631acbc04eb4f3e6950d8aa00048
Piyojo <piyojo@gmail.com>
2020-03-05 22:51:43 +0100

    lazy comment

--- setup.js
+++ setup.js
@@ -1,17 +1,32 @@
 
 
-var sos;
-var button;
+let sos;
+let button;
+let header;
+let bg;
 
 // todo: style the app with p5.dom
+// todo: flexbox, media queries, verwoert art, button-style, font, named sliders1   
 // todo: spiral icon? 
 // todo: later, make a bass oscillator with filter and resonance
 function setup() {
     sos = loadSound('./selfoscillation.mp3', loaded);
-    button = createButton('play'); 
+    header1 = createDiv('gimmick');
+    header2 = createDiv('time');
+    header3 = createDiv('volume');
+    header4 = createDiv('rate');
+    button = createButton('play');
     button.mousePressed(togglePlaying);  
     sliderVolume = createSlider(0, 1, 0.5, 0.01);
     sliderRate = createSlider(0, 3, 1, 0.01);
+    header1.position(150, 30);
+    header2.position(120, 60)
+    button.position(130, 210); 
+    sliderVolume.position(300, 160);
+    header3.position(260, 130);
+    header4.position(220, 300);
+    sliderRate.position(230, 330);
+
 }
 
 function togglePlaying() {
