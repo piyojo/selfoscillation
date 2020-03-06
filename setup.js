38a193ed1d34fbb8d5a1045a2cdfa85afb903dee
Piyojo <piyojo@gmail.com>
2020-03-07 00:03:57 +0100

    lazy comment

--- setup.js
+++ setup.js
@@ -1,41 +1,39 @@
 
-
-let sos;
-let button;
-let header;
-let bg;
-
-// todo: style the app with p5.dom
-// todo: flexbox, media queries, verwoert art, button-style, font, named sliders1   
-// todo: spiral icon? 
+// todo: style with p5.dom
+// todo: icon 
 // todo: later, make a bass oscillator with filter and resonance
+
+let sound;
+let button;
+
 function setup() {
-    sos = loadSound('./selfoscillation.mp3', loaded);
-    header1 = createDiv('gimmick');
-    header2 = createDiv('time');
-    header3 = createDiv('volume');
-    header4 = createDiv('rate');
-    button = createButton('play');
-    button.mousePressed(togglePlaying);  
+    sound = loadSound('./selfoscillation.mp3', loaded);
+
+    gimmickWord = createDiv('gimmick');
+    timeWord = createDiv('time');
+    volumeWord = createDiv('volume');
+    rateWord = createDiv('rate');
     sliderVolume = createSlider(0, 1, 0.5, 0.01);
     sliderRate = createSlider(0, 3, 1, 0.01);
-
-    header1.position(70, 30);
-    header2.position(40, 60)
+    
+    button = createButton('play');
+    button.mousePressed(togglePlaying); ;
     button.position(50, 210); 
+    
+    gimmickWord.position(70, 30);
+    timeWord.position(40, 60)
+    volumeWord.position(190, 130);
+    rateWord.position(140, 300);
     sliderVolume.position(220, 160);
-    header3.position(190, 130);
-    header4.position(140, 300);
     sliderRate.position(150, 330);
-
 }
 
 function togglePlaying() {
-    if(!sos.isPlaying()) {
-    sos.play();
+    if(!sound.isPlaying()) {
+    sound.play();
     button.html('stop');
     } else {
-        sos.stop();
+        sound.stop();
         button.html('play');
     }
 }
@@ -45,8 +43,8 @@ console.log('loaded');
 }
 
 function draw() {
-    sos.setVolume(sliderVolume.value());
-    sos.rate(sliderRate.value());
+    sound.setVolume(sliderVolume.value());
+    sound.rate(sliderRate.value());
 }
 
-console.log('kati is lief');
+
